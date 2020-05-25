c91cbc0e0a020632c0bad6347b971ac5e0f5b91a
Admin <Admin@CS-681.mshome.net>
2020-05-25 14:26:58 +0900

    message

--- src/TomokiTakami/Main.java
+++ src/TomokiTakami/Main.java
@@ -1,5 +1,9 @@
-package TomokiTakami;
 
 public class Main {
-
+	public static void main(String[]args) {
+		Hero h1 = new Hero("菅原");
+		System.out.println(h1.name);
+		Hero h2 = new Hero();
+		System.out.println(h2.name);
+	}
 }
---
+++ src/TomokiTakami/renshu01.java
@@ -0,0 +1,5 @@
+package src.TomokiTakami;
+
+public class renshu01 {
+
+}
