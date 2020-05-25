2b9e75143d0b3685b1c5fb2a3619d357fc8b3408
yuji.awa <yuji.awa@CS-678.mshome.net>
2020-05-25 16:28:29 +0900

    commit

---
+++ src/yujiAwa/renshu01/Thief.java
@@ -0,0 +1,15 @@
+package yujiAwa.renshu01;
+
+public class Thief {
+	String name;
+	int hp;
+	int mp;
+	public Thief(String name, int hp, int mp) {
+	}
+	public Thief(String name,int hp) {
+		this(name,hp,5);
+	}
+	public Thief(String name) {
+		this(name,40,5);
+	}
+}
